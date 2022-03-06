277433d13c556d2bc54af1636465b69cca600daa
cawac <cawapl33@gmail.com>
2022-03-06 19:44:50 +0300

    create window application, with stations and railways beetwen, and goods on them

--- main.py
+++ main.py
@@ -1,148 +1,12 @@
-# Snake Tutorial Using Pygame
-import math
-import random
+import time
+from time import *
 import pygame
-import tkinter as tk
-from tkinter import messagebox
-
-
-class cube(object):
-    rows = 20
-    w = 500
-
-    def __init__(self, start, dirnx=1, dirny=0, color=(255, 0, 0)):
-        self.pos = start
-        self.dirnx = 1
-        self.dirny = 0
-        self.color = color
-
-    def move(self, dirnx, dirny):
-        self.dirnx = dirnx
-        self.dirny = dirny
-        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
-
-    def draw(self, surface, eyes=False):
-        dis = self.w // self.rows
-        i = self.pos[0]
-        j = self.pos[1]
-
-        pygame.draw.rect(surface, self.color, (i * dis + 1, j * dis + 1, dis - 2, dis - 2))
-        if eyes:
-            centre = dis // 2
-            radius = 3
-            circleMiddle = (i * dis + centre - radius, j * dis + 8)
-            circleMiddle2 = (i * dis + dis - radius * 2, j * dis + 8)
-            pygame.draw.circle(surface, (0, 0, 0), circleMiddle, radius)
-            pygame.draw.circle(surface, (0, 0, 0), circleMiddle2, radius)
-
-        # This class is defined for snake design and its movement
-
-
-class snake(object):
-    body = []
-    turns = {}
-
-    def __init__(self, color, pos):
-        self.color = color
-        self.head = cube(pos)
-        self.body.append(self.head)
-        self.dirnx = 0
-        self.dirny = 1
-
-    def move(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-
-            keys = pygame.key.get_pressed()
-            # It will manage the keys movement for the snake
-            for key in keys:
-                if keys[pygame.K_LEFT]:
-                    self.dirnx = -1
-                    self.dirny = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-
-                elif keys[pygame.K_RIGHT]:
-                    self.dirnx = 1
-                    self.dirny = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-
-                elif keys[pygame.K_UP]:
-                    self.dirnx = 0
-                    self.dirny = -1
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-
-                elif keys[pygame.K_DOWN]:
-                    self.dirnx = 0
-                    self.dirny = 1
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-                    # Snake when hit the boundary wall
-        for i, c in enumerate(self.body):
-            p = c.pos[:]
-            if p in self.turns:
-                turn = self.turns[p]
-                c.move(turn[0], turn[1])
-                if i == len(self.body) - 1:
-                    self.turns.pop(p)
-            else:
-                if c.dirnx == -1 and c.pos[0] == c.rows - 1:
-                    c.pos = (0, c.pos[1])
-                elif c.dirny == 1 and c.pos[1] >= c.rows - 1:
-                    c.pos = (c.pos[0], 0)
-                elif c.dirny == -1 and c.pos[1] == 0:
-                    continue
-                else:
-                    break
-        return (x, y)
-
-
-# Using Tkinter function to display message
-def message_box(subject, content):
-    root = tk.Tk()
-    root.attributes("-topmost", True)
-    root.withdraw()
-    messagebox.showinfo(subject, content)
-    try:
-        root.destroy()
-    except:
-        pass
-
-    # main() function
-
-
-def main():
-    global width, rows, s, snack
-    width = 500
-    rows = 20
-    win = pygame.display.set_mode((width, width))
-    s = snake((255, 0, 0), (10, 10))
-    snack = cube(randomSnack(rows, s), color=(0, 255, 0))
-    flag = True
-
-    clock = pygame.time.Clock()
-
-    while flag:
-        pygame.time.delay(50)
-        clock.tick(10)
-        s.move()
-        if s.body[0].pos == snack.pos:
-            s.addCube()
-            snack = cube(randomSnack(rows, s), color=(0, 255, 0))
-
-        for x in range(len(s.body)):
-            if s.body[x].pos in list(map(lambda z: z.pos, s.body[x + 1:])):
-                print('Score: \n', len(s.body))
-                message_box('You Lost!\n', 'Play again...\n')
-                s.reset((10, 10))
-                break
-
-        redrawWindow(win)
-
-    pass
 
 
 class Station:
-    def __init__(self, name, capacity=5, size=25):
+    def __init__(self, name, posx, posy, capacity=5, size=25, ):
+        self.__posx = posx
+        self.__posy = posy
         self.__capacity = capacity
         self.__size = size
         self.__name = name
@@ -171,13 +35,29 @@ class Station:
     def name(self, x):
         self.__name = x
 
+    @property
+    def posx(self):
+        return self.__posx
+
+    @posx.setter
+    def posx(self, new_posx):
+        self.__posx = new_posx
+
+    @property
+    def posy(self):
+        return self.__posy
+
+    @posy.setter
+    def posy(self, new_posy):
+        self.__posy = new_posy
+
 
 class Game_map:
     def __init__(self):
-        a = Station('a')
-        b = Station('b')
-        c = Station('c')
-        d = Station('d')
+        a = Station('a', 40, 40)
+        b = Station('b', 140, 140)
+        c = Station('c', 40, 140)
+        d = Station('d', 140, 40)
         self.__stations = [a, b, c, d]
         self.__graph = {self.__stations[0]: [self.__stations[1], self.__stations[2]],
                         self.__stations[1]: [self.__stations[2], self.__stations[3]]}
@@ -214,12 +94,13 @@ class Train:
             print(station.name, end=' ')
         key = int(input())
         self.__curr_station = self.__game_map.graph[self.__curr_station][key]
+        sleep(3)
 
     def loading(self):
         print('на станции сейчас столько грузов:', self.__curr_station.capacity)
         print('в поезде сейчас столько груза:', self.capacity)
         kolvo = int(input('введите сколько товара забрать со станции: '))
-        while kolvo < 0 or self.__curr_station.scapacity - kolvo < 0:
+        while kolvo < 0 or self.__curr_station.capacity - kolvo < 0:
             print('на станции сейчас столько грузов:', self.__curr_station.capacity)
             print('в поезде сейчас столько груза:', self.capacity)
             kolvo = int(input('введите сколько товара забрать со станции: '))
@@ -230,6 +111,7 @@ class Train:
             buf = self.__size - self.__capacity
             self.__capacity = self.__size
             self.__curr_station.capacity += -buf + kolvo
+        sleep(1)
 
     def unloading(self):
         print('на станции сейчас столько грузов:', self.__curr_station.capacity)
@@ -241,6 +123,7 @@ class Train:
             kolvo = int(input('введите сколько товара выгрузить на станцию: '))
         self.__capacity -= kolvo
         self.__curr_station.capacity += kolvo
+        sleep(1)
         # self.__curr_station.capacity(self.__curr_station.capacity + kolvo)
 
     @property
@@ -253,13 +136,36 @@ class Train:
 
 
 if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('train model')
+    win = pygame.display.set_mode((500, 500))
     x = Game_map()
-    train = Train(x)
-    train.go_to_station()
-    print(train.curr_station.name)
-    train.loading()
-    print(train.curr_station.capacity)
-    print(train.capacity)
-    train.unloading()
-    print(train.curr_station.capacity)
-    print(train.capacity)
+    # train = Train(x)
+    # train.go_to_station()
+    # print(train.curr_station.name)
+    # train.loading()
+    # print(train.curr_station.capacity)
+    # print(train.capacity)
+    # train.unloading()
+    # print(train.curr_station.capacity)
+    # print(train.capacity)
+    run = True
+
+    while run:
+        pygame.time.delay(100)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        win.fill((0, 0, 0))
+        for station in x.stations:
+            pygame.draw.circle(win, (140, 200, 10), (station.posx, station.posy), 20)
+            for i in range(station.capacity):
+                pygame.draw.rect(win, (200, 200, 200), (station.posx + 20 + i * 7, station.posy - 20, 5, 5))
+        for start in x.graph:
+            for end in x.graph[start]:
+                pygame.draw.aaline(win, (255, 255, 255),
+                                   [start.posx, start.posy],
+                                   [end.posx, end.posy])
+
+        pygame.display.update()
+    pygame.quit()
