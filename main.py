90f56caa0fa7bbb882f1a18bef150a9d01d9cc11
cawac <cawapl33@gmail.com>
2022-04-01 23:58:57 +0300

    add diagrams and annotations

--- main.py
+++ main.py
@@ -1,245 +1,184 @@
 import random
 import time
 import pygame
+import typing
+
+width: int = 860
+height: int = 600
+bg = pygame.image.load("back.jpg")
 
 
 class Station:
-    def __init__(self, name, posx, posy, size=25):
+    def __init__(self, name: str, posx: int, posy: int, size: int, capacity: int):
+        self.__name = name
         self.__posx = posx
         self.__posy = posy
-        self.__capacity = random.randint(1, 7)
         self.__size = size
-        self.__name = name
+        self.__capacity = capacity
 
     @property
-    def size(self):
+    def size(self) -> int:
         return self.__size
 
-    @size.setter
-    def size(self, x):
-        self.__size = x
-
     @property
-    def capacity(self):
+    def capacity(self) -> int:
         return self.__capacity
 
     @capacity.setter
-    def capacity(self, x):
-        self.__capacity = x
+    def capacity(self, value: int) -> None:
+        self.__capacity = value
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.__name
 
-    @name.setter
-    def name(self, x):
-        self.__name = x
-
     @property
-    def posx(self):
+    def posx(self) -> int:
         return self.__posx
 
     @posx.setter
-    def posx(self, new_posx):
+    def posx(self, new_posx: int) -> None:
         self.__posx = new_posx
 
     @property
-    def posy(self):
+    def posy(self) -> int:
         return self.__posy
 
     @posy.setter
-    def posy(self, new_posy):
+    def posy(self, new_posy: int) -> None:
         self.__posy = new_posy
+
+
+def is_touch(x1: int, y1: int, x2: int, y2: int) -> bool:
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5 < 60
+
+
+def is_clicked(x1: int, y1: int, x2: int, y2: int) -> bool:
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5 < 20
 
 
 class Game_map:
     def __init__(self):
-        a = Station('a', 40, 40)
-        b = Station('b', 240, 240)
-        c = Station('c', 240, 40)
-        d = Station('d', 40, 240)
-        e = Station('e', 360, 120)
-        f = Station('f', 360, 360)
-        self.__stations = [a, b, c, d, e, f]
-        self.__graph = {self.__stations[0]: [self.__stations[1], self.__stations[2]],
-                        self.__stations[1]: [self.__stations[0], self.__stations[3], self.__stations[4]],
-                        self.__stations[2]: [self.__stations[0], self.__stations[5]],
-                        self.__stations[3]: [self.__stations[1]],
-                        self.__stations[4]: [self.__stations[1], self.__stations[5]],
-                        self.__stations[5]: [self.__stations[2], self.__stations[4]]}
+        self.__stations = [Station('a', 100, 100, 25, random.randint(2, 8))]
+        for i in range(1, random.randint(11, 14)):
+            generate(self)
+        self.__graph = {}
+        for station_start in self.__stations:
+            self.__graph[station_start] = []
+        for station_start in self.__stations:
+            for j in range(random.randint(1, len(self.__stations) // 2)):
+                new_station = random.choice(self.__stations)
+                if new_station not in self.__graph[station_start]:
+                    self.__graph[station_start].append(new_station)
+                    self.__graph[new_station].append(station_start)
 
-    def spawn(self):
-        self.__stations[random.randint(0, 5)].capacity += 5
+    def spawn(self) -> None:
+        random.choice(self.__stations).capacity += 5
 
     @property
-    def graph(self):
+    def graph(self) -> dict:
         return self.__graph
 
     @property
-    def stations(self):
+    def stations(self) -> typing.List[Station]:
         return self.__stations
 
-    @graph.setter
-    def graph(self, x):
-        self.__graph = x
-
-    @stations.setter
-    def stations(self, x):
-        self.__stations = x
-
-    def is_overload(self):
+    def is_overload(self) -> bool:
         for statione in self.__stations:
             if statione.capacity >= statione.size:
                 return True
-            else:
-                return False
+        else:
+            return False
+
+
+def generate(mapa: Game_map) -> None:
+    x = random.randint(30, width - 50)
+    y = random.randint(30, height - 30)
+    for i in mapa.stations:
+        if is_touch(i.posx, i.posy, x, y):
+            generate(mapa)
+            return
+    mapa.stations.append(Station(chr(97 + len(mapa.stations)), x, y, 25, random.randint(2, 8)))
 
 
 class Train:
-    def __init__(self, game_map, size=25, capacity=0):
+    def __init__(self, game_map: Game_map) -> None:
         self.__game_map = game_map
         self.__curr_station = game_map.stations[0]
-        self.__size = size
-        self.__capacity = capacity
         self.__posx = float(self.__curr_station.posx)
         self.__posy = float(self.__curr_station.posy)
-        self.__cosa = 1.0
 
-    def go_to_station(self):
-        for statione in self.__game_map.graph[self.__curr_station]:
-            print(statione.name, end=' ')
-        start = self.__curr_station
-        key = int(input())
-        self.__curr_station = self.__game_map.graph[self.__curr_station][key]
-        end = self.__curr_station
-        self.__cosa = abs(start.posx - end.posx) / (abs(start.posx - end.posx) ** 2 + abs(start.posy - end.posy) ** 2) ** 0.5
-        first_time = time.time()
-        while time.time() - first_time <= 3:
-            pygame.time.delay(50)
-            self.__posx += (end.posx - start.posx) / 60
-            self.__posy += (end.posy - start.posy) / 60
+    def go_to_station(self, x: int, y: int) -> None:
+        for key in self.__game_map.graph[self.__curr_station]:
+            if is_clicked(x, y, key.posx, key.posy):
+                start = self.__curr_station
+                self.__curr_station = key
+                end = key
+                # self.__cosa = abs(start.posx - end.posx) / (
+                # (start.posx - end.posx) ** 2 + (start.posy - end.posy) ** 2) ** 0.5
+                first_time = time.time()
+                while time.time() - first_time <= 3:
+                    pygame.time.delay(50)
+                    self.__posx += (end.posx - start.posx) / 45
+                    self.__posy += (end.posy - start.posy) / 45
+                    draw_window(self, self.__game_map)
+                self.__posx = self.__curr_station.posx
+                self.__posy = self.__curr_station.posy
+
+    def loading(self, x: int, y: int) -> None:
+        if is_clicked(x, y, self.__curr_station.posx, self.__curr_station.posy):
+            self.__curr_station.capacity -= 1
             draw_window(self, self.__game_map)
-        self.__posx = self.__curr_station.posx
-        self.__posy = self.__curr_station.posy
-
-    def loading(self):
-        print('на станции сейчас столько грузов:', self.__curr_station.capacity)
-        print('в поезде сейчас столько груза:', self.capacity)
-        kolvo = int(input('введите сколько товара забрать со станции: '))
-        while kolvo < 0 or self.__curr_station.capacity - kolvo < 0:
-            print('на станции сейчас столько грузов:', self.__curr_station.capacity)
-            print('в поезде сейчас столько груза:', self.capacity)
-            kolvo = int(input('введите сколько товара забрать со станции: '))
-        if self.__capacity + kolvo <= self.__size:
-            self.__capacity += kolvo
-            self.__curr_station.capacity -= kolvo
-        else:
-            buf = self.__size - self.__capacity
-            self.__capacity = self.__size
-            self.__curr_station.capacity += -buf + kolvo
-        time.sleep(kolvo)
-
-    def unloading(self):
-        print('на станции сейчас столько грузов:', self.__curr_station.capacity)
-        print('в поезде сейчас столько груза:', self.capacity)
-        kolvo = int(input('введите сколько товара выгрузить на станцию: '))
-        while kolvo < 0 or self.__capacity - kolvo < 0:
-            print('на станции сейчас столько грузов:', self.__curr_station.capacity)
-            print('в поезде сейчас столько груза:', self.capacity)
-            kolvo = int(input('введите сколько товара выгрузить на станцию: '))
-        self.__capacity -= kolvo
-        self.__curr_station.capacity += kolvo
-        time.sleep(kolvo)
-        # self.__curr_station.capacity(self.__curr_station.capacity + kolvo)
 
     @property
-    def curr_station(self):
+    def curr_station(self) -> Station:
         return self.__curr_station
 
     @property
-    def capacity(self):
-        return self.__capacity
+    def curr_station(self) -> Station:
+        return self.__curr_station
 
     @property
-    def posx(self):
+    def posx(self) -> float:
         return self.__posx
 
-    @property
-    def posy(self):
+    @property	
+    def posy(self) -> float:
         return self.__posy
 
-    @property
-    def cosa(self):
-        return self.__cosa 
+    
 
-# def menu(x, train):
-#     first_time = time.time()
-#     if ((time.time() - first_time) > 20):
-#         x.spawn()
-#         first_time = time.time()
-#     print(f'Поезд на [' + train.curr_station.name + '] станции')
-#     print('Что сделать?')
-#     key = input('(1) Следующая станция\n(2) Загрузить поезд\n(3) Разгрузить поезд\n')
-#     if key == '1':
-#         train.go_to_station()
-#         print('едем...')
-#         time.sleep(2)
-#     elif key == '2':
-#         train.loading()
-#         print('загружаю...')
-#         time.sleep(3)
-#     elif key == '3':
-#         train.unloading()
-#         print('разгружаю...')
-#         time.sleep(3)
-#     if x.check_overloading()
-#         print('ПОТРАЧЕНО')
-
-
-def calc_point_x(x, y, a, b, cosa, sina):
-    return (x - a) * cosa - (y - b) * sina + a
-
-
-def calc_point_y(x, y, a, b, cosa, sina):
-    return (x - a) * sina + (y - b) * cosa + b
-
-
-def draw_window(train, x):  # draw display
-    win.fill((0, 0, 0))  # fill background
-    for start in x.graph:  # drawing railways
-        for end in x.graph[start]:
-            pygame.draw.aaline(win, (255, 255, 255),
+def draw_window(the_Thomas: Train, railway: Game_map) -> None:  # draw display
+    win.blit(bg, (0, 0))
+    for start in railway.graph:  # drawing railways
+        for end in railway.graph[start]:
+            pygame.draw.aaline(win, (255, 202, 0),
                                [start.posx, start.posy],
                                [end.posx, end.posy])
-    for station in x.stations:  # drawing stations
+    for station in railway.stations:  # drawing stations
         j = 0
-        pygame.draw.circle(win, (140, 200, 10),
+        pygame.draw.circle(win, (255, 159, 0),
                            (station.posx, station.posy), 20)
+        f1 = pygame.font.Font(None, 24)
+        text1 = f1.render(station.name, True,
+                          (15, 79, 168))
+        win.blit(text1, (station.posx - 10, station.posy - 10))
         for i in range(station.capacity):  # drawing goods
             if i % 5 == 0:
                 j += 1
-            pygame.draw.rect(win, (200, 200, 200),
+            pygame.draw.rect(win, (225, 0, 93),
                              (station.posx + 21 + i % 5 * 7, station.posy - 30 + j * 7, 5, 5))
-    pygame.draw.rect(win, (13, 85, 166), (train.posx, train.posy, 10, 10))        # need to draw train
-    # sina = (1 - train.cosa ** 2) ** 0.5
-    # pygame.draw.polygon(win, (13, 85, 166),
-    #                     [[calc_point_x(30, 20, train.posx, train.posy, sina, train.cosa), calc_point_y(30, 20, train.posx, train.posy, sina, train.cosa)],
-    #                      [calc_point_x(30, 20, train.posx, train.posy, sina, train.cosa), calc_point_y(30, 20, train.posx, train.posy, sina, train.cosa)],
-    #                      [calc_point_x(30, 20, train.posx, train.posy, sina, train.cosa), calc_point_y(30, 20, train.posx, train.posy, sina, train.cosa)],
-    #                      [calc_point_x(30, 20, train.posx, train.posy, sina, train.cosa), calc_point_y(30, 20, train.posx, train.posy, sina, train.cosa)]])
-    # pygame.draw.aalines(sc, WHITE, True,
-    #                     [[250, 110], [280, 150],
-    #                      [190, 190], [130, 130]])
+    pygame.draw.rect(win, (133, 122, 238), (the_Thomas.posx, the_Thomas.posy, 10, 10))
     pygame.display.update()  # update display
 
 
 if __name__ == '__main__':
     pygame.init()
+    pygame.font.init()
     pygame.display.set_caption('train model')
-    win = pygame.display.set_mode((500, 500))
+    win = pygame.display.set_mode((width, height))
     railway_map = Game_map()
     train = Train(railway_map)
-
     first_time = time.time()
     while not railway_map.is_overload():
         pygame.time.delay(50)
@@ -247,18 +186,19 @@ if __name__ == '__main__':
         if (time.time() - first_time) > 1:
             railway_map.spawn()
             first_time = time.time()
-        print(f'Поезд на [' + train.curr_station.name + '] станции')
-        print('Что сделать?')
-        key = input('(1) Следующая станция\n(2) Загрузить поезд\n(3) Разгрузить поезд\n')
-        if key == '1':
-            print('едем...')
-            train.go_to_station()
-        elif key == '2':
-            print('загружаю...')
-            train.loading()
-        elif key == '3':
-            print('разгружаю...')
-            train.unloading()
+        key = 0
+        ev = pygame.event.get()
+        for event in ev:
+            # handle MOUSEBUTTONUP
+            if railway_map.is_overload():
+                break
+            if event.type == pygame.MOUSEBUTTONUP:
+                x, y = pygame.mouse.get_pos()
+                key = event.button
+                if key == 1:
+                    train.loading(x, y)
+                elif key == 3:
+                    train.go_to_station(x, y)
     if railway_map.is_overload():
         print('ПОТРАЧЕНО')
 
